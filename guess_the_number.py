5c65680d2aa4faee1a72433470035c05ea9d44a4
DeeStAcE <dawidfirmowski@gmail.com>
2023-03-26 11:19:33 +0200

    guess the number

--- guess_the_number.py
+++ guess_the_number.py
@@ -5,16 +5,12 @@ app = Flask(__name__, template_folder="./templates")
 
 @app.route("/", methods=["POST", 'GET'])
 def guess_the_number():
-
-    # print(guess)
     if request.method == "POST":
         min_ = request.form.get('min')
         max_ = request.form.get('max')
-        print(min_, max_)
-        min_ = float(min_)
-        max_ = float(max_)
-        print(min_, max_)
-        guess = int((max_ - min_)) / 2 + min_
+        min_ = int(min_)
+        max_ = int(max_)
+        guess = int((max_ - min_) / 2 + min_)
         if request.form.get('too_small'):
             return render_template("html_guess.html", min_=guess, max_=max_)
         if request.form.get('too_big'):
@@ -22,6 +18,9 @@ def guess_the_number():
         if request.form.get('you_win'):
             return 'You win!'
     if request.method == "GET":
+        # min_ = 0
+        # max_ = 1000
+        # guess = int((max_ - min_)) / 2 + min_
         return render_template("html_guess.html", min_=0, max_=1000)
 
 
